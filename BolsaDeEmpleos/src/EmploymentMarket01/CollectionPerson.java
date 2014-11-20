385d2be6e2a0ec05ac59019bf5eaf4a963f82424
Weed03 <miguel.abreu03@gmail.com>
2014-11-20 16:26:02 -0400

    Second one

---
+++ BolsaDeEmpleos/src/EmploymentMarket01/CollectionPerson.java
@@ -0,0 +1,18 @@
+package EmploymentMarket01;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class CollectionPerson {
+	private ArrayList<Person> Persons;
+	
+	public CollectionPerson(ArrayList<Person> persons) {
+		super();
+		Persons = persons;
+	}
+
+	public ArrayList<Person> getPersons() {
+		return Persons;
+	}
+	
+}
