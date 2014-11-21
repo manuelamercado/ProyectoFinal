81bb90a84a34423b8d0419725afe8a991a8d83c6
Weed03 <miguel.abreu03@gmail.com>
2014-11-21 11:35:48 -0400

    CollectionPerson(Ended)

--- BolsaDeEmpleos/src/EmploymentMarket01/CollectionPerson.java
+++ BolsaDeEmpleos/src/EmploymentMarket01/CollectionPerson.java
@@ -2,6 +2,7 @@ package EmploymentMarket01;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class CollectionPerson {
 	private ArrayList<Person> Persons;
@@ -14,5 +15,18 @@ public class CollectionPerson {
 	public ArrayList<Person> getPersons() {
 		return Persons;
 	}
+
+	public void setPersons(Person persons) {
+		Persons.add(persons);
+		Order(Persons);
+	}
 	
+	public void Order(ArrayList<Person> Persons){
+		Collections.sort(Persons, new Comparator<Person>(){
+			@Override
+			public int compare(Person p1, Person p2){
+				return new String(p1.getLastName()).compareTo(p2.getLastName());
+			}
+		});
+	}
 }
